c5b28bbf09b21bb43b48bea0109042f81eb22fc2
Max Bogue <max@maxbogue.com>
2012-05-22 17:10:52 -0400

    Tiny alignment fix.

--- MarkovColoring.hs
+++ MarkovColoring.hs
@@ -139,7 +139,7 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of 
-         [fn, es, algo] | Just e <- maybeRead es -> do
+        [fn, es, algo] | Just e <- maybeRead es -> do
             (n, m, q, g) <- readGraph fn
             let colors = [1..q]
             count <- case algo of
