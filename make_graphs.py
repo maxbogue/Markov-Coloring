133a42417e3cf84e17144e5148379cff9649f764
Max Bogue <max@maxbogue.com>
2012-05-22 21:55:34 -0400

    Update the make_graphs.py script to accept a q parameter.

--- make_graphs.py
+++ make_graphs.py
@@ -4,7 +4,7 @@ from sys import argv
 from random import sample
 from collections import Counter
 
-def print_graph(n, m):
+def print_graph(n, m, q):
     counter = Counter()
     edges = set([])
     for _ in range(m):
@@ -13,7 +13,7 @@ def print_graph(n, m):
             edge = tuple(sorted(sample(range(n), 2)))
         edges.add(edge)
         counter.update(list(edge))
-    q = counter.most_common()[0][1] + 3
+    q = max(q, counter.most_common(1)[0][1] + 2)
     print "%s %s %s" % (n, m, q)
     for edge in sorted(edges):
         print "%s %s" % edge
@@ -21,4 +21,5 @@ def print_graph(n, m):
 if __name__ == "__main__":
     n = int(argv[1])
     m = int(argv[2])
-    print_graph(n, m)
+    q = int(argv[3])
+    print_graph(n, m, q)
